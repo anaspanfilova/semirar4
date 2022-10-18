f1d5fd5d5ebf8ead894c1cda3c57870c4ba427fe
Анастасия <understooooood@mail.ru>
2022-10-18 13:38:44 +0300

    решили 30 задачу с правкой

---
+++ 30 норм/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит массим из 8 элементов, заполненный нулями и еденицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+
+
+
+int[] RandomArr()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+
+
+}
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"{mass[i]}");
+        if (i != mass.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+
+int [] N = RandomArr();
+PrintArray(N);
