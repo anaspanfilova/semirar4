7430bc0e979707b81a9de807641f5adf9bc85057
Анастасия <understooooood@mail.ru>
2022-10-18 09:48:53 +0300

    решили 24 задачу

---
+++ 24/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до (А).
+//
+
+int GetSum (int A)
+{
+    int sum = 0;
+    
+    for (int i = 1; i <= A; i++)
+    {
+        sum += i; // sum = sum + 1;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetSum(n));
