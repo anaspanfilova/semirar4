61024adae0129735896affba01b0ea70f34143fa
Анастасия <understooooood@mail.ru>
2022-10-18 10:38:05 +0300

    решили 28 задачу с правкой

---
+++ 28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+int GetProduct (int n)
+{
+    int Product = 1;
+    
+    for (int i = 1; i <= n; i++)
+    {
+        Product = Product * i;
+    }
+    return Product;
+}
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetProduct(n));
