1477f39c368e319d24a1a748242906b10657a52b
Анастасия <understooooood@mail.ru>
2022-10-18 15:03:26 +0300

    решили 27 задачу

---
+++ Домашняя задача №27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите функцию и запустите ее, которая
+// принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum (int a)
+{
+    int sum = 0;
+    
+    while(a > 0 || a < 0)
+    {
+        sum = sum + (a % 10);
+        a = a / 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Sum(n));
